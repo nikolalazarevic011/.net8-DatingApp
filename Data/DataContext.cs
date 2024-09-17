77c902d4a55cd7fffd8dac8d71745d079bc56047
nikolalazarevic011 <nikola.lazaa@gmail.com>
2024-09-17 17:02:16 -0500

    end of sec 15

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,6 +8,9 @@ public class DataContext(DbContextOptions options) : DbContext(options)
     public DbSet<AppUser> Users { get; set; }
 
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
+
+
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -27,5 +30,15 @@ public class DataContext(DbContextOptions options) : DbContext(options)
             .WithMany(l => l.LikedByUsers)
             .HasForeignKey(s => s.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Message>()
+       .HasOne(x => x.Recipient)
+       .WithMany(x => x.MessagesReceived)
+       .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Message>()
+            .HasOne(x => x.Sender)
+            .WithMany(x => x.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -14,7 +14,11 @@ namespace api.Helpers
                 .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url)); //!  "!" 91. Configuring AutoMapper. min 2:40. telling compiler to disregard if it's null , cuz autammper will do it for us, we know it does that
 
             CreateMap<Photo, PhotoDto>();
-
+            CreateMap<Message, MessageDto>()
+                        .ForMember(d => d.SenderPhotoUrl,
+                            o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain)!.Url))
+                        .ForMember(d => d.RecipientPhotoUrl,
+                            o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain)!.Url));
 
         }
     }
